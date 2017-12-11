6b938b79459777a18da2700cbf874ffdee6efe4d
xue <1677587143@qq.com>
2017-12-11 09:14:47 +0800

    投资pc

---
+++ src/js/index.js
@@ -0,0 +1,12 @@
+import $ from 'jquery';
+
+
+
+import "../css/normailze.css"
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.js'
+
+import 'Swiper/dist/css/swiper.css';
+import Swiper from 'Swiper/dist/js/swiper.js';
+import "../css/index.css"
